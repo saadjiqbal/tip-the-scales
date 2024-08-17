1696f25ddb7e42152f3f9b48203cc6a48c969ab8
nashtonash <41835529+nashtonash@users.noreply.github.com>
2024-08-17 01:55:49 -0400

    initial batch of sprites imported

---
+++ custom_resources/object_dictionary.gd
@@ -0,0 +1,15 @@
+extends Resource
+
+var object_dict: Dictionary = {
+	"template" : {
+		"name" : "object_name",
+		"flavor" : "flavor_text",
+		"target_scale" : 1.0,
+	},
+	"apple" : {
+		"name" : "apple",
+		"flavor" : "Gross, one of the red ones.",
+		"target_scale" : .1,
+	},
+	
+}
