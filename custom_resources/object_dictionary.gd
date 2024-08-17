adf391265091a2242416bb867465371691736837
nashtonash <41835529+nashtonash@users.noreply.github.com>
2024-08-17 03:21:34 -0400

    base_object scene setup

--- custom_resources/object_dictionary.gd
+++ custom_resources/object_dictionary.gd
@@ -3,12 +3,12 @@ extends Resource
 var object_dict: Dictionary = {
 	"template" : {
 		"name" : "object_name",
-		"flavor" : "flavor_text",
+		"description" : "flavor_text",
 		"target_scale" : 1.0,
 	},
-	"apple" : {
-		"name" : "apple",
-		"flavor" : "Gross, one of the red ones.",
+	"Apple" : {
+		"name" : "Apple",
+		"description" : "Gross, one of the red ones.",
 		"target_scale" : .1,
 	},
 	
--- scenes/objects/base_object.gd
+++ scenes/objects/base_object.gd
@@ -1,12 +1,18 @@
-extends AnimatableBody2D
+extends StaticBody2D
 
-class_name BaseObject
+# Access to the object_dictionary.gd resource to get data on the object
+# Used so that we don't need to create a new script for each object
+@export var object_dict: Resource
 
-# Called when the node enters the scene tree for the first time.
+var object_name: String = ""
+var object_text: String = ""
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# On ready, set object_name and object_text based on the name of the\
+	# inherited scene
+	
+	# Using [self.name]["name"] seems repetitive and not quite necessary,
+	# but it felt right to have "name" as one of the values in the object's
+	# entry
+	object_name = object_dict[self.name]["name"]
+	object_text = object_dict[self.name]["description"]
