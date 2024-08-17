7813c0ac0f15ba9012f68cb82a75e1d957bb02aa
nashtonash <41835529+nashtonash@users.noreply.github.com>
2024-08-17 17:07:53 -0400

    conveyor belt setup

--- scenes/objects/base_object.gd
+++ scenes/objects/base_object.gd
@@ -1,18 +1,7 @@
 extends StaticBody2D
 
-# Access to the object_dictionary.gd resource to get data on the object
-# Used so that we don't need to create a new script for each object
-@export var object_dict: Resource
+@export var target_scale: float
 
-var object_name: String = ""
-var object_text: String = ""
-
-func _ready() -> void:
-	# On ready, set object_name and object_text based on the name of the\
-	# inherited scene
-	
-	# Using [self.name]["name"] seems repetitive and not quite necessary,
-	# but it felt right to have "name" as one of the values in the object's
-	# entry
-	object_name = object_dict[self.name]["name"]
-	object_text = object_dict[self.name]["description"]
+func _physics_process(delta: float) -> void:
+	if (not is_on_floor()):
+		global_position.y += 50 * delta
