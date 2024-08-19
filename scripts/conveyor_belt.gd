2b3518f9673783c485086f343e5cf716e5807479
Portland <portlandhorton01@proton.me>
2024-08-19 17:07:12 -0600

    Conveyor Bug fixes

--- scripts/conveyor_belt.gd
+++ scripts/conveyor_belt.gd
@@ -12,16 +12,6 @@ func _physics_process(delta: float) -> void:
 	detect_object(delta)
 
 func detect_object(delta):
-	if object_detector.is_colliding():
-		part_under_teleporter = true
-		self.stop()
-		
-		if audio_stream_player.playing:
-			audio_stream_player.playing = false
-	
-	if object_detector.is_colliding() == false:
-		part_under_teleporter = false
-	
 	if !part_under_teleporter:
 		self.play("roll")
 		belt_collision.position.x += speed * delta
@@ -32,3 +22,14 @@ func detect_object(delta):
 	# Resets the belt colosion box back to its original position
 	if belt_collision.position.x >= 1193.0:
 		belt_collision.position.x = 0.0
+
+
+func _on_object_detector_area_entered(area: Area2D) -> void:
+	part_under_teleporter = true
+	self.stop()
+	if audio_stream_player.playing:
+		audio_stream_player.playing = false
+
+func _on_object_detector_area_exited(area: Area2D) -> void:
+	self.play("roll")
+	part_under_teleporter = false
