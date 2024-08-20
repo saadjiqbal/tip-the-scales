b9296bec492358e46047a84503b1906e70db4727
nashtonash <41835529+nashtonash@users.noreply.github.com>
2024-08-20 03:30:00 -0400

    damage bug fixed

--- scripts/health_ui.gd
+++ scripts/health_ui.gd
@@ -4,23 +4,12 @@ extends Control
 	preload("res://scenes/lose_screen.tscn")
 @onready var health_sprites: Control = $HealthSprites
 
-
-var health: int = Global.health
-
-func _ready() -> void:
-	Global.health = 5
+var health: int = 5
 
 # Free the rightmost health indicator, and load lose_screen.tscn if health == 0
-func _on_game_damage_taken():
+func _on_scanner_damaged() -> void:
 	health -= 1
 	if health == 0:
 		get_tree().change_scene_to_packed(lose_screen)
 	else:
 		health_sprites.get_child(health).queue_free()
-
-# Debug to test health loss functionality. Don't turn back on
-func _process(_delta) -> void:
-	if Global.damage:
-		_on_game_damage_taken()
-		Global.damage = false
-		print(health)
--- scripts/scanner.gd
+++ scripts/scanner.gd
@@ -13,6 +13,8 @@ var min_scale : Vector2 = Vector2(0.4, 0.35)
 
 var object_name
 
+signal damaged()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -23,21 +25,22 @@ func _process(delta: float) -> void:
 	if $RayCast2D.is_colliding():
 	
 		#This gets the scale of the sprite connected to the object
-		var object_scale = $RayCast2D.get_collider().get_child(0).scale
-		if object_scale < max_scale && object_scale > min_scale:
-			$RayCast2D.get_collider().queue_free()
-			$Good_job.play()
-		else:
-			$RayCast2D.get_collider().queue_free()
-			$Bad_job.play()
-			#Appends the part list array to include the bad part
-			translate_name_to_part()
-			get_parent().can_appand = true
-			get_parent().append_array()
-			get_parent().can_appand = false
-			#Reduces the machines durability
-			Global.health -= 1
-			Global.damage = true
+		var object_scale: Vector2
+		if $RayCast2D.get_collider() != null:
+			object_scale = $RayCast2D.get_collider().get_child(0).scale
+			if object_scale < max_scale && object_scale > min_scale:
+				$RayCast2D.get_collider().queue_free()
+				$Good_job.play()
+			else:
+				$RayCast2D.get_collider().queue_free()
+				$Bad_job.play()
+				#Appends the part list array to include the bad part
+				translate_name_to_part()
+				get_parent().can_appand = true
+				get_parent().append_array()
+				get_parent().can_appand = false
+				#Reduces the machines durability
+				damaged.emit()
 		
 		print(object_scale)
 		print(object_name)
